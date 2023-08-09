32fbf2e3242a13c52470db27855b0e41d699165f
iurialb <iurialbuquerque@protonmail.com>
2023-08-09 16:16:37 -0300

    lista

--- Fiap_Listas_1.py
+++ Fiap_Listas_1.py
@@ -1,18 +1,9 @@
 ### Projeto inicial sobre listas pela FIAP
 
-def preencherInventario(lista):
-  resp="S"
-  while resp == "S":
-    equipamento=[input("Equipamento: "),
-              float(input("Valor: ")),
-              int(input("Número Serial: ")),
-              input("Departamento: ")]
-    lista.append(equipamento)
-    resp = input("Digite "S" para continuar: ").upper()
-
-def exibirInventario(lista):
+def localizarPorNome(lista):
+  busca=input("
+Digite o nome do equipamento que deseja buscar: ")
   for elemento in lista:
-    print("Nome.........: ", elemento[0])
-    print("Valor........: ", elemento[1])
-    print("Serial.......: ", elemento[2])
-    print("Departamento.: ", elemento[3])
+    if busca==elemento[0]:
+      print("Valor..: ", elemento[1])
+      print("Serial.:", elemento[2])
